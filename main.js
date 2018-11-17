6a692a7f77e52dfbf0f212c05fba2d00377300ed
Aliaksandr Bychak <aliaksandr.bychak@gmail.com>
2018-11-17 11:02:10 +0300

    add validate pass, ref code

--- main.js
+++ main.js
@@ -13,40 +13,49 @@ function validateForm(){
     validateInput(mail);
     let password = document.getElementById('password');
     validateInput(password);
-    let confirm = document.getElementById('confirm');
-
 }
 
 function validateInput(input){
-    input.classList.remove('err');
-    
+    removeErrorStyle(input);
+    let errorMessage = {
+        Pattern: '" is not validated!',
+        Required: '" is required!',
+        ConfirmedPass: '" does not match the password!'
+    };
+
     if (!isRequiredField(input)){
-        createErrorRequired(input);
-    } else {
-        if (isFieldPattern(input)) {
-            var pattern = input.getAttribute('pattern');
-            var reg = new RegExp(pattern);
-            if (!reg.test(input.value.toUpperCase())) { 
-                createErrorPattern(input);
-            } 
-          }
+        showError(input, errorMessage.Required);
+    } else if (isFieldPattern(input)) {
+        var pattern = input.getAttribute('pattern');
+        var reg = new RegExp(pattern);
+        if (!reg.test(input.value.toUpperCase())) { 
+            showError(input, errorMessage.Pattern);
+        } else if (input.id === password.id)
+            validatePassword(input, errorMessage.ConfirmedPass);
     }
+
 }
 
-function createErrorPattern(input){
+function validatePassword(password, errorMessage){
+    let confirm = document.getElementById('confirm');
+    if (!(password.value === confirm.value)){
+        return showError(confirm, errorMessage);
+    } else 
+        removeErrorStyle(confirm);
+}
+
+function showError(input, errorType){
     let errorItem = document.createElement('li');
     errorItem.className = 'error-item';
-    errorItem.textContent = 'field "' + input.name + '" is not validated!';
+    errorItem.textContent = 'field "' + input.name + errorType;
     input.classList.add('err');
+    input.parentNode.classList.add('error-star');
     document.querySelector('.result').appendChild(errorItem);
 }
 
-function createErrorRequired(input){
-    let errorItem = document.createElement('li');
-    errorItem.className = 'error-item';
-    errorItem.textContent = 'field "' + input.name + '" is required!';
-    input.classList.add('err');
-    document.querySelector('.result').appendChild(errorItem);
+function removeErrorStyle(input){
+    input.classList.remove('err');
+    input.parentNode.classList.remove('error-star');
 }
 
 function isFieldPattern(input){
@@ -61,5 +70,3 @@ function clearErrorArea(){
     let result = document.querySelector('.result');
     result.innerHTML = '';        
 }
-
-//if(e.target.classlist.has('class'))
